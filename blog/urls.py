877f862911616258d461e86b2baf49b11469c8b0
Renzo <castiglione.renzo@gmail.com>
2022-09-13 22:58:32 -0300

    modifico templates html

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from blog.views import *
 urlpatterns = [
+    path('', inicio, name='inicio'),
     path('inicio', inicio, name='inicio'),
     path('login', login, name='login'),
 ]
